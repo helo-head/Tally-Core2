874d5bb4c1a871df79702b48aa611593dd101cae
Glenn Christiansen <39964639+helo-head@users.noreply.github.com>
2023-01-31 15:41:38 -0600

    Update Tallly-Core2-V11.ino
    Resolution to macro button events

--- Tallly-Core2-V11.ino
+++ Tallly-Core2-V11.ino
@@ -767,26 +767,33 @@ String ip2Str(IPAddress ip) {
   return s;
 }  // End of ip2St
 
-// Button press function
+/// Button press function
 void buttonWasPressed(Event &e) {
   Button &tbtn = *e.button;
   const char *buttonName = e.objName();
   char buttonType = buttonName[0];
   int buttonNumber = buttonName[1] - '0';
 
-  if (!postConfig) {
+  if (!postConfig) {                  //Update file selection button appropriatly
     fileNumber = buttonName[1] - 48;  // Hack to convert ascii char to long
     M5.Lcd.fillRect(tbtn.x, tbtn.y, tbtn.w, tbtn.h, tbtn.isPressed() ? WHITE : BLACK);
 
   } else {
-
+  if ( e.type == E_TOUCH) {           // If a touch event set ATEM appropriatly and update the button if a maco
     if (buttonType == 'c') {
       AtemSwitcher.changeProgramInput(buttonNumber);
     } else {
-      updateMacroButton(buttonNumber, macroOffset, tbtn.isPressed(), false);
+      updateMacroButton(buttonNumber, macroOffset, tbtn.isPressed());
       AtemSwitcher.setMacroAction(buttonNumber + macroOffset, 0);
     }
+  } else {                            // Release handler for macros to update button appropriatly
+    if (buttonType == 'm') {
+      updateMacroButton(buttonNumber, macroOffset, tbtn.isPressed());
+    }
   }
+    
+  } //End camera/macro button handling
+  
 }  // End buttonWasPressed
 
 // Draw camera buttons
@@ -831,18 +838,21 @@ void drawCameraButton(int buttonNumber, int state) {
 }  // End drawCameraButton
 
 // Update Macro button function
-void updateMacroButton(int buttonNumber, int macroOffset, bool isPressed, bool isRunning) {
+void updateMacroButton(int buttonNumber, int macroOffset, bool isPressed) {
   int font = 1;
   M5.Lcd.setTextSize(6);
 
-  M5.Lcd.setTextColor(isPressed || isRunning ? BLACK : WHITE);
-  int buttonColor = isPressed ? WHITE : (isRunning ? TFT_RED : TFT_DARKGREY);
+  M5.Lcd.setTextColor(isPressed  ? BLACK : WHITE);
+  int buttonColor = (isPressed ? WHITE : TFT_DARKGREY);
+  
+  TFT_DARKGREY);
 
   buttonNumber = buttonNumber + macroOffset;
 
   switch (buttonNumber) {
     case 0:
       M5.Lcd.fillRoundRect(buttonOneLocationX, MACRO_BUTTON_Y, BUTTON_SIZE, BUTTON_SIZE, 6, buttonColor);
+      
       M5.Lcd.drawCentreString("0", 2 + buttonOneLocationX + (BUTTON_SIZE / 2), MACRO_BUTTON_Y + 18, font);
       break;
     case 1:
@@ -968,7 +978,7 @@ void setup() {
 
   Serial.begin(115200);
   // Define the touch handler
-  M5.Buttons.addHandler(buttonWasPressed, E_TAP + E_TOUCH + E_RELEASE);
+  M5.Buttons.addHandler(buttonWasPressed, E_TOUCH + E_RELEASE);
 
   // Setup the SD card
   if (!SD.begin()) {
